1d7ff9c454ca6dda9b1cdd2040fe9438d2cf91d2
Matt Gaikema <mgaikema1@gmail.com>
2017-03-06 17:03:45 -0600

    it actually works, my computer just sucks

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-// a simple TeX-input example
 var mjAPI = require("mathjax-node");
 var fs = require('fs');
-var jsdom = require("jsdom").jsdom;
+
+/*
+TODO:
+1. Create a module for converting LaTeX to SVG
+2. Create a webserver with a url endpoint that takes a TeX string as a GET request and returns an SVG string.
+3. Something something telegram.
+*/
 
 mjAPI.config({
 	MathJax: {
 		SVG : {
-			scale: 120,
+			scale: 240,
 			font : "STIX-Web",
 			linebreaks: { automatic: true },
 			tex2jax: { 
@@ -26,12 +31,18 @@ function fsWriteCallback(err){
 }
 
 var yourMath = String.raw`\frac{1}{e}`;
+console.log(yourMath);
 mjAPI.typeset({
 	math: yourMath,
 	format: "TeX",
-	svg: true
+	svg: true,
+	width: 5
 }, function (data) {
 	if (!data.errors) {
+		/*
+		For some reason I can only view the image in the browser.
+		I cannot see it with Ubuntu's image viewer.
+		*/
 		fs.writeFile("data.svg", data.svg, fsWriteCallback);
 	}
 });
