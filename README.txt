3d8e857d294ae66ed408edbd6e90990d01d81f1f
Matt Gaikema <mgaikema1@gmail.com>
2017-03-06 14:09:29 -0600

    initial commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+A web app which renders LaTeX to SVG, and also does stuff in Telegram.
+
+https://github.com/mathjax/MathJax-node
+https://www.npmjs.com/package/mathjax
+http://stackoverflow.com/q/42240973/5415895
---
+++ server.js
@@ -0,0 +1,32 @@
+// a simple TeX-input example
+var mjAPI = require("mathjax-node");
+var fs = require('fs');
+var jsdom = require("jsdom").jsdom;
+
+mjAPI.config({
+	MathJax: {
+		SVG : {
+			scale: 120,
+			font : "STIX-Web",
+			linebreaks: { automatic: true },
+			tex2jax: { 
+				inlineMath: [['$','$'], ['\\(','\\)']],
+			}
+		}
+	}
+});
+mjAPI.start();
+
+var yourMath = '\frac{1}{e}';
+mjAPI.typeset({
+	math: yourMath,
+	format: "TeX",
+	svg: true,
+	inputs : ["TeX"],
+	renderer: "SVG",
+	svgNode: true
+}, function (data) {
+	if (!data.errors) {
+		fs.writeFile("data.svg", data.svg);
+	}
+});
