ce20d5e88028e425ac85611bdd79f4776bff1985
Matt Gaikema <mgaikema1@gmail.com>
2017-03-06 20:36:04 -0600

    bind to port

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.get('/', function (req, res) {
 });
 
 // Run server.
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
