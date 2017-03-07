ee95d921f10b26bb0a6f41a71c0203ca6696ea84
Matt Gaikema <mgaikema1@gmail.com>
2017-03-06 19:48:13 -0600

    simple example works

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var fs = require('fs');
 var renderLatex = require('./renderLatex.js');
+var express = require('express');
+var app = express();
 
 /*
 TODO:
-1. Create a module for converting LaTeX to SVG
-2. Create a webserver with a url endpoint that takes a TeX string as a GET request and returns an SVG string.
+1. Create a module for converting LaTeX to SVG. DONE
+2. Create a webserver with a url endpoint that takes a TeX string as a GET request and returns an SVG string. http://stackoverflow.com/a/17008027/5415895
 3. Something something telegram.
 */
 
@@ -13,8 +15,22 @@ var input = String.raw`\frac{1}{e}`;
 renderLatex.tex2svg(input, function(str){
 	fs.writeFile("data.svg", str, function (err){
 		if (err){
-			return console.log(err);
+			console.log(err);
 		}
 		console.log('File successfully written.');
 	});
+});
+
+
+// This returns an svg 1/e.
+app.get('/', function (req, res) {
+	res.setHeader('Content-Type', 'image/svg+xml');
+	renderLatex.tex2svg(input, function(s){
+		res.send(s);
+	});
+});
+
+// Run server.
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
